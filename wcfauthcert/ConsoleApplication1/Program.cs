3232eb19472b1457fdee6440460d3737fbf44e27
umut senoglu <umut_senoglu@hotmail.com>
2015-11-18 18:00:55 +0200

    1

---
+++ wcfauthcert/ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new ServiceReference1.Service1Client();
+            var value = client.GetData(77);
+            Console.WriteLine(value);
+            Console.ReadLine();
+        }
+    }
+}
